36d027037fafaf6c9a3c77e38641b7a4f19b93da
aleksandrsokolov <timac11@bk.ru>
2025-10-13 17:43:16 +0300

    minor correction

--- internal/handler/default.go
+++ internal/handler/default.go
@@ -4,11 +4,6 @@ import (
 	"net/http"
 )
 
-const (
-	Counter = "counter"
-	Gauge   = "gauge"
-)
-
 func NotFound(res http.ResponseWriter, req *http.Request) {
 	http.Error(res, "Not found", http.StatusNotFound)
 }
