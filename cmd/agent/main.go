efa4826149d29208f47668b67b9db8a99251aef0
aleksandrsokolov <timac11@bk.ru>
2025-11-01 23:20:24 +0900

    resolve some discussions

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"github.com/timac11/musthave-metrics-collector/internal/agent"
 	"github.com/timac11/musthave-metrics-collector/internal/config"
-	"github.com/timac11/musthave-metrics-collector/internal/logger"
 )
 
 func main() {
 	flags := config.InitAgentFlags()
-	logger.Log(flags.Collectnterval)
-	logger.Log(flags.WriteInterval)
 	metricsAgent := agent.NewMetricsAgent(flags)
 	metricsAgent.Start()
 }
