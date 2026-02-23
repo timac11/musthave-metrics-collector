aa01251892f42ad3d41e791a48542bb26c241de7
aleksandrsokolov <timac11@bk.ru>
2026-02-23 23:03:55 +0300

    minor correction

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -23,14 +23,14 @@ func Initialize(level string) error {
 	return nil
 }
 
-func Debug(msg string, params ...interface{}) {
+func Debug(msg string, params any) {
 	logger.Debug(msg, params)
 }
 
-func Info(msg string, params ...interface{}) {
+func Info(msg string, params any) {
 	logger.Info(msg, params)
 }
 
-func Error(msg string, params ...interface{}) {
+func Error(msg string, params any) {
 	logger.Error(msg, params)
 }
