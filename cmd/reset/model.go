c68b9c257696c74e596b7d445fa1b19010b52d4a
aleksandrsokolov <timac11@bk.ru>
2026-03-10 21:43:14 +0300

    updates

---
+++ cmd/reset/model.go
@@ -0,0 +1,26 @@
+package reset
+
+import (
+	"go/ast"
+)
+
+type packageInfo struct {
+	Name string
+	Dir  string
+	Ast  []*ast.File
+}
+
+type genInfo struct {
+	Name  string
+	Funcs []string
+}
+
+type structInfo struct {
+	Name   string
+	Fields []string
+}
+
+type structAstInfo struct {
+	Name string
+	Typ  *ast.StructType
+}
