a20f4f50ea0fa0bac6afe53553192ecfed315226
aleksandrsokolov <timac11@bk.ru>
2026-03-11 00:01:41 +0300

    add version printing

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/timac11/musthave-metrics-collector/internal/agent"
 	"github.com/timac11/musthave-metrics-collector/internal/config"
 	"github.com/timac11/musthave-metrics-collector/internal/logger"
+
+	"github.com/timac11/musthave-metrics-collector/cmd/version"
 )
 
 func main() {
@@ -17,6 +19,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	version.Print()
 
 	metricsAgent.Start()
 }
